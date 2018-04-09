c0f31b752b18bfef38f83e20478bcf186efc11a0
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-04-08 23:53:17 -0400

    fixed proportions?

--- src/components/myCompanies/MyCompanyDetail.js
+++ src/components/myCompanies/MyCompanyDetail.js
@@ -95,7 +95,7 @@ class MyCompanyDetail extends React.Component {
       <h3>{this.state.company.size}</h3>
         <div style={{fontSize:"18px", fontWeight:"400"}}>{this.state.company.description}</div>
 </div>
-  <img src={this.state.company.image_link} style={{width:"480px", height:"100%", float:"left", display: "block", minWidth:"280px", overflowX:"hidden", margin:"0.25em", padding:"0.25em", flexDirection:"row", order:"2", flex:"flex-shrink"}} />
+  <img src={this.state.company.image_link} style={{width:"480px", height:"100%", float:"left", display: "block", minWidth:"320px", maxWidth:"480px", overflowX:"hidden", margin:"0.25em", padding:"0.25em", flexDirection:"row", order:"2", flex:"flex-shrink"}} />
 </div>
 
 
